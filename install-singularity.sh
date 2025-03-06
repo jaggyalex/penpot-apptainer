d51abee4c78229d4384fda1060ca6b5d14c6eb02
Alexander Perminov <alexander.perminov.ctr@us.af.mil>
2025-03-06 14:06:05 -0500

    Added script for installing singularity.

---
+++ install-singularity.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# My system is Ubuntu.
+# https://docs.sylabs.io/guides/4.2/user-guide/quick_start.html
+
+# Ensure repositories are up-to-date
+sudo apt-get update
+# Install debian packages for dependencies
+sudo apt-get install -y \
+   autoconf \
+   automake \
+   cryptsetup \
+   fuse2fs \
+   git \
+   fuse \
+   libfuse-dev \
+   libglib2.0-dev \
+   libseccomp-dev \
+   libtool \
+   pkg-config \
+   runc \
+   squashfs-tools \
+   squashfs-tools-ng \
+   uidmap \
+   wget \
+   zlib1g-dev
+
+# Install go
+export VERSION=1.22.6 OS=linux ARCH=amd64 && \
+  wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+  sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+  rm go$VERSION.$OS-$ARCH.tar.gz
+
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
+  source ~/.bashrc
+
+# Install Singularity CE
+export VERSION=4.2.0 && \
+    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-ce-${VERSION}.tar.gz && \
+    tar -xzf singularity-ce-${VERSION}.tar.gz && \
+    cd singularity-ce-${VERSION}
+
+./mconfig && \
+    make -C builddir && \
+    sudo make -C builddir install
